3b8c5905ac3411679747d3117b85990c48b15533
Isaac <icorbrey@gmail.com>
2021-12-08 19:16:27 -0500

    feat: Create fuzzy searcher

--- DDI.WebApp/wwwroot/js/site.js
+++ DDI.WebApp/wwwroot/js/site.js
@@ -1,4 +1,38 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$("#drugSearch").on("input", function ()
+{
+	const query = $(this).val()
+	const items = $('.drug-name')
+		.toArray()
+		.map(x => x.innerText)
 
-// Write your JavaScript code.
+	const fuse = new Fuse(items, {
+		includeScore: true,
+	})
+
+	if (0 < query?.length ?? 0)
+	{
+		const matchingItems = fuse.search(query)
+		const results = matchingItems.map(x => x.item)
+		const scores = matchingItems
+			.map(({ item, score }) => ({ [item]: score }))
+			.reduce((x, y) => ({ ...x, ...y }), {})
+
+		$(".drug-name")
+			.filter(function () { return results.includes($(this).text()) })
+			.parent()
+			.show()
+
+		$(".drug-name")
+			.filter(function () { return !results.includes($(this).text()) })
+			.parent()
+			.hide()
+	}
+	else
+	{
+		$(".drug-name")
+			.parent()
+			.show()
+	}
+})
+
+const toSortResult = (result) => result ? 1 : 0
